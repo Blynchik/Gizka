b6d50e378723230e33b089d08cb54b6620f46d55
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-07-04 19:50:29 +0300

    divided on entity and rest

--- rest/src/main/java/project/gizka/exception/notFound/AppUserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package project.gizka.exception.notFound;
-
-public class AppUserNotFoundException extends RuntimeException{
-
-    public AppUserNotFoundException(String message){
-        super(message);
-    }
-}

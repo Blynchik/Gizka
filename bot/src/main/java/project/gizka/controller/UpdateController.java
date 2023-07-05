721c072ff7cfffcdd56ef144cde557ae282aa927
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-07-05 12:24:54 +0300

    bot can get all exceptions

--- bot/src/main/java/project/gizka/controller/UpdateController.java
+++ bot/src/main/java/project/gizka/controller/UpdateController.java
@@ -1,13 +1,13 @@
 package project.gizka.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.HashMap;
+import java.util.Objects;
 
 @Component
 public class UpdateController {
@@ -16,27 +16,41 @@ public class UpdateController {
 
     String url = "http://localhost:8080/api/user/";
 
-    public void processUpdate(Update update){
-        if(update == null){
+    public void processUpdate(Update update) {
+        if (update == null) {
             return;
         }
 
-        if(update.hasMessage()){
+        if (update.hasMessage()) {
             Message originalMessage = update.getMessage();
             String text = originalMessage.getText();
             final RestTemplate restTemplate = new RestTemplate();
-            HashMap response = restTemplate.getForObject(url + text, HashMap.class);
-            System.out.println(response);
-            SendMessage message = new SendMessage();
-            message.setChatId(originalMessage.getChatId().toString());
-            message.setText(response.toString());
-            telegramBot.sendAnswerMessage(message);
-        } else {
+           try{
+                var response = restTemplate.getForEntity(url + text, Object.class);
+                String responseBody = Objects.requireNonNull(response.getBody()).toString();
+                SendMessage message = new SendMessage();
+                message.setChatId(originalMessage.getChatId().toString());
+                message.setText(responseBody);
+                telegramBot.sendAnswerMessage(message);
+            } catch (HttpClientErrorException.NotFound e){
+                String responseBody = e.getResponseBodyAsString();
+                SendMessage message = new SendMessage();
+                message.setChatId(originalMessage.getChatId().toString());
+                message.setText(responseBody);
+                telegramBot.sendAnswerMessage(message);
+            } catch (Exception e){
+               String responseBody = e.getMessage();
+               SendMessage message = new SendMessage();
+               message.setChatId(originalMessage.getChatId().toString());
+               message.setText(responseBody);
+               telegramBot.sendAnswerMessage(message);
+           }
+        }else {
             return;
         }
     }
 
-    public void setBot(TelegramBot telegramBot){
+    public void setBot(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
     }
 }

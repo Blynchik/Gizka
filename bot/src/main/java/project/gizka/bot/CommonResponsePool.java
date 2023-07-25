37bbf68286291d7bf71ae8ff1fc4a079840d6d5c
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-07-25 09:24:20 +0300

    delay added

--- bot/src/main/java/project/gizka/bot/CommonResponsePool.java
+++ bot/src/main/java/project/gizka/bot/CommonResponsePool.java
@@ -5,8 +5,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 @Getter
@@ -15,31 +16,43 @@ public class CommonResponsePool {
     private final PrivateResponsePools privateResponsePools;
     private static CommonResponsePool instance;
     private static final int THREADS = 2;
+    private ScheduledExecutorService executorService;
 
-    private CommonResponsePool(){
+    private CommonResponsePool() {
         commonPool = new ConcurrentLinkedQueue<>();
         this.privateResponsePools = PrivateResponsePools.getInstance();
+        executorService = Executors.newScheduledThreadPool(THREADS);
     }
 
-    public static synchronized  CommonResponsePool getInstance(){
-        if(instance == null){
+    public static synchronized CommonResponsePool getInstance() {
+        if (instance == null) {
             instance = new CommonResponsePool();
         }
         return instance;
     }
 
     public void addPrivatePoolToCommonPool() {
-        ExecutorService executor = Executors.newFixedThreadPool(THREADS);
-        for(String key : privateResponsePools.getPrivateResponsePools().keySet()){
+        for (String key : privateResponsePools.getPrivateResponsePools().keySet()) {
             Queue<SendMessage> messages = privateResponsePools.getPrivateResponsePools().get(key);
-            privateResponsePools.getPrivateResponsePools().remove(key);
+            int delay = 0;
 
-            executor.submit(() -> {
-                synchronized (commonPool) {
-                    commonPool.addAll(messages);
+            for (SendMessage message : messages) {
+                if (messages.size() > 1) {
+                    executorService.schedule(() -> {
+                        synchronized (commonPool) {
+                            commonPool.add(message);
+                        }
+                    }, delay, TimeUnit.SECONDS);
+                    delay += 3;
+                } else {
+                    synchronized (commonPool) {
+                        commonPool.addAll(messages);
+                    }
                 }
-            });
+            }
+
+            privateResponsePools.getPrivateResponsePools().remove(key);
         }
-        executor.shutdown();
     }
 }
+
